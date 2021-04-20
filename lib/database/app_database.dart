41ec5f6eb8c611006dc4ee554464487b72c40760
Prof Rafael <rafa.fernan10@gmail.com>
2021-04-20 15:43:29 -0300

    Refatoração inicial no BD

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,48 +2,44 @@ import 'package:flutter_files/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  //o then é necessário para tratar o Future
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(
-      path,
-      onCreate: (db, version) {
-        db.execute('CREATE TABLE tb_contacts('
-            'id INTEGER PRIMARY KEY, '
-            'name TEXT, '
-            'account_number INTEGER)');
-      },
-      version: 1,
-      //onDowngrade: onDatabaseDowngradeDelete, //código usado para apagar o BD após realizar downgrade
+Future<Database> createDatabase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, 'bytebank.db');
+  //final String path = join(await getDatabasesPath(), 'bytebank.db'); // MESMO CÓDIGO DAS DUAS LINHAS ACIMA
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute('CREATE TABLE tb_contacts('
+          'id INTEGER PRIMARY KEY, '
+          'name TEXT, '
+          'account_number INTEGER)');
+    },
+    version: 1,
+    //onDowngrade: onDatabaseDowngradeDelete, //código usado para apagar o BD após realizar downgrade
+  );
+}
+
+Future<int> save(Contact contact) async {
+  final Database db = await createDatabase();
+  Map(); //qual a utilidade desta linha?
+  final Map<String, dynamic> contactMap = Map();
+  //contactMap['id'] = contact.id; linha não utilizada para geração automática de ID
+  contactMap['name'] = contact.nome;
+  contactMap['account_number'] = contact.conta;
+  return db.insert('tb_contacts', contactMap);
+}
+
+Future<List<Contact>> findAll() async {
+  final Database db = await createDatabase();
+  final List<Map<String, dynamic>> resultado = await db.query('tb_contacts');
+  final List<Contact> listaContatos = [];
+  for (Map<String, dynamic> row in resultado) {
+    final Contact contact = Contact(
+      row['id'],
+      row['name'],
+      row['account_number'],
     );
-  });
-}
-
-Future<int> save(Contact contact) {
-  return createDatabase().then((db) {
-    Map();
-    final Map<String, dynamic> contactMap = Map();
-    //contactMap['id'] = contact.id; linha não utilizada para geração automática de ID
-    contactMap['name'] = contact.nome;
-    contactMap['account_number'] = contact.conta;
-    return db.insert('tb_contacts', contactMap);
-  });
-}
-
-Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
-    return db.query('tb_contacts').then((maps) {
-      final List<Contact> listaContatos = [];
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact = Contact(
-          map['id'],
-          map['name'],
-          map['account_number'],
-        );
-        listaContatos.add(contact);
-      }
-      return listaContatos;
-    });
-  });
+    listaContatos.add(contact);
+  }
+  return listaContatos;
 }
